c83af0f0e7ed15562fc16f939a3010e4d619de8c
Harun Demir <harun.demir918@outlook.com>
2021-08-28 15:00:06 +0300

    tarif sayfası için link eklendi, avatar değiştirildi

--- resources/views/layouts/_partials/sidenav.blade.php
+++ resources/views/layouts/_partials/sidenav.blade.php
@@ -1,14 +1,15 @@
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
     <aside class="app-sidebar">
-      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image">
+      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://icons.iconarchive.com/icons/papirus-team/papirus-status/256/avatar-default-icon.png" alt="User Image" width="64" height="64">
         <div>
           <p class="app-sidebar__user-name">{{ auth()->user()->name }}</p>
           <p class="app-sidebar__user-designation">{{ auth()->user()->role }}</p>
         </div>
       </div>
       <ul class="app-menu">
-        <li><a class="app-menu__item active" href="{{ route('home') }}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
-        <li><a class="app-menu__item" href="{{ route('users') }}"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Users</span></a></li>
+        <li><a class="app-menu__item active" href="{{ route('home') }}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Ana Sayfa</span></a></li>
+        <li><a class="app-menu__item" href="{{ route('recipes') }}"><i class="app-menu__icon fa fa-cutlery"></i><span class="app-menu__label">Tarifler</span></a></li>
+        <li><a class="app-menu__item" href="{{ route('users') }}"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Kullanıcılar</span></a></li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
             <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
