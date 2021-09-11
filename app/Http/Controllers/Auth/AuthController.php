defc3b005e598c2b7a482c41e93ccd666829c926
Harun Demir <harun.demir918@outlook.com>
2021-09-11 14:54:12 +0300

    login fonksiyonu güncellendi

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,10 +6,11 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Notifications\ConfirmYourAccount;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-     /**
+    /**
      * Create user
      *
      * @param  [string] name
@@ -37,7 +38,7 @@ class AuthController extends Controller
             'message' => 'Successfully created user!'
         ], 201);
     }
-  
+
     /**
      * Login user and create token
      *
@@ -50,35 +51,14 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-            'remember_me' => 'boolean'
-        ]);
-        $credentials = request(['email', 'password']);
-        $credentials['active'] = 1;
-        $credentials['deleted_at'] = null;
-        if (!Auth::attempt($credentials)) {
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
+        $loginData = $request->validate(['email' => 'email|required', 'password' => 'required']);
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
         }
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me) {
-            $token->expires_at = Carbon::now()->addWeeks(1);
-            $token->save();
-        }
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ]);
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        return response()->json(['user' => auth()->user(), 'access_token' => $accessToken]);
     }
-  
+
     /**
      * Logout user (Revoke the token)
      *
@@ -91,7 +71,7 @@ class AuthController extends Controller
             'message' => 'Successfully logged out'
         ]);
     }
-  
+
     /**
      * Get the authenticated User
      *
@@ -116,6 +96,6 @@ class AuthController extends Controller
         if (request()->wantsJson()) {
             return $user;
         }
-        return redirect('/home')->with('success','Accounted activated successfully');
+        return redirect('/home')->with('success', 'Accounted activated successfully');
     }
 }
